c70a99df1a79e2af8d2cf4b905bd69a37e46156c
郝高明 <978235424@qq.com>
2018-04-19 12:11:34 +0800

    创建barrier
    创建barrier

--- GCD-Test/GCD-Swift.swift
+++ GCD-Test/GCD-Swift.swift
@@ -39,4 +39,64 @@ extension GCD_Swift {
         //utility：用于长时间执行的任务，比如app中会有一个进度条表示任务的进行，主要用于计算、IO、网络交互等。低优先级
         //background：任务在运行，但是用户感觉不到的场景，这种queue是后台优先级。最低优先级
     }
+    
+    /// 创建custom queue：默认为串行队列，但是如果指定了attributes为concurrent，则变为并行队列
+    func DispatchQueue_CustomQueue() {
+        //创建串行队列
+        DispatchQueue(label: "com.queue").async {
+            print("customQueue的串行队列")
+        }
+        
+        //创建并行队列
+        let queue = DispatchQueue(label: "com.queue", qos: DispatchQoS.default, attributes: DispatchQueue.Attributes.concurrent, autoreleaseFrequency: DispatchQueue.AutoreleaseFrequency.inherit, target: nil)
+        queue.async {
+            print("customQueue的并行队列")
+        }
+        //默认创建的时候，可以使用  DispatchQueue(label: "com.queue", attributes: DispatchQueue.Attributes.concurrent),上边的方法为完整的传参方法创建
+        
+        //其中，autoreleaseFrequency：顾名思义，自动释放频率。有些队列是会在履行完任务后自动释放的，有些比如Timer等是不会自动释放的，是需要手动释放。
+        //以前，DispatchQueues将在未指定的时间（当线程变为不活动时）弹出它们的自动释放池。 在实践中，这意味着您为每个提交的调度项目创建了一个自动释放池，或者您的自动释放的对象将挂起不可预测的时间量。
+        //非确定性不是什么好事情（特别是在并发库！），所以现在Apple允许你指定三种行为之一：
+        //.inherit：不确定，之前默认的行为也是现在的默认值
+        //.workItem：为每个执行的项目创建和排除自动释放池,项目完成时清理临时对象
+        //.never：GCD不为您管理自动释放池
+    }
 }
+
+// MARK: - 创建barrier
+extension GCD_Swift
+{
+    func dispatch_barrier() {
+        //创建barrier
+        let barrier = DispatchWorkItem(flags: DispatchWorkItemFlags.barrier) {
+            sleep(3)
+            print("barrier执行")
+        }
+        DispatchQueue(label: "com.artron.queue", attributes: .concurrent).async(execute: barrier)
+        
+        //barrier：用于等待前面的任务执行完毕后自己才执行，而barrier之后的任务需等待它执行完成之后才执行。
+        //典型例子：数据的读写，通常为了防止文件读写导致冲突，我们会创建一个串行队列，所有的读取文件都在串行队列里来执行。
+        //FMDB：众所周知，FMDB是一个并行操作，但是它是怎么保证写入的时候是串行执行，读取的时候是并行执行呢？
+        let queue = DispatchQueue(label: "queue", attributes: .concurrent)
+        queue.async {
+            print("reading 1")
+        }
+        queue.async(execute: barrier)
+        queue.async {
+            sleep(2)
+            print("reading 2")
+        }
+        queue.async {
+            print("reading 3")
+        }
+        //输出结果为：
+        //reading 1
+        //barrier执行
+        //reading 3
+        //reading 2
+        //从上可以看出，barrier的执行sleep了3秒，这是一个并行队列，而reading2和reading3都在barrier之后执行，所以验证了上边的论证。
+    }
+}
+
+
+
